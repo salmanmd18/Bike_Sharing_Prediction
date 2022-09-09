e18ca84bf8c43655f07176485c8db04a17a4c9cf
Mohammed Salman <salmanmohammed2018@gmail.com>
2022-09-09 19:06:41 +0530

    Installed requirements

---
+++ demo.py
@@ -0,0 +1,22 @@
+from sharing.config.configuration import Configuration
+from sharing.pipeline.pipeline import Pipeline
+from sharing.exception import SharingException
+from sharing.logger import logging
+import os,sys
+
+def main():
+    try:
+        
+        config_path = os.path.join("config","config.yaml")
+        pipeline = Pipeline(Configuration(config_file_path=config_path))
+        data_validation_config = Configuration().get_data_validation_config()
+        print(data_validation_config)
+        pipeline.run_pipeline()
+        print("fd")
+    except Exception as e:
+        logging.error(f"{e}")
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
