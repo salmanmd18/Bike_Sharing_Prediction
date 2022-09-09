9a0240e50d799e52787a495fd8ee12298b56154f
Mohammed Salman <salmanmohammed2018@gmail.com>
2022-09-09 19:19:37 +0530

    Added docker and heroku

--- demo.py
+++ demo.py
@@ -1,22 +1,14 @@
-from sharing.config.configuration import Configuration
-from sharing.pipeline.pipeline import Pipeline
-from sharing.exception import SharingException
-from sharing.logger import logging
-import os,sys
+from flask import Flask
 
-def main():
-    try:
-        
-        config_path = os.path.join("config","config.yaml")
-        pipeline = Pipeline(Configuration(config_file_path=config_path))
-        data_validation_config = Configuration().get_data_validation_config()
-        print(data_validation_config)
-        pipeline.run_pipeline()
-        print("fd")
-    except Exception as e:
-        logging.error(f"{e}")
-        print(e)
+app = Flask(__name__)
 
 
-if __name__ == "__main__":
-    main()
+@app.route("/",methods = ["GET","POST"])
+def index():
+    #return "Starting Maching Learning Project"
+    return "CI/CD pipeline has been established"
+
+
+
+if  __name__=="__main__":
+    app.run(debug=True)
