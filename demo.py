8964d5ea70990cd163ee1df86d3a69d8f192963a
Mohammed Salman <salmanmohammed2018@gmail.com>
2022-09-09 19:42:05 +0530

    API Tested

--- demo.py
+++ demo.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from sharing.config.configuration import Configuration
+from sharing.pipeline.pipeline import Pipeline
+from sharing.exception import SharingException
+from sharing.logger import logging
+import os,sys
 
-app = Flask(__name__)
+def main():
+    try:
+        
+        config_path = os.path.join("config","config.yaml")
+        pipeline = Pipeline(Configuration(config_file_path=config_path))
+        data_validation_config = Configuration().get_data_validation_config()
+        print(data_validation_config)
+        pipeline.run_pipeline()
+        print("fd")
+    except Exception as e:
+        logging.error(f"{e}")
+        print(e)
 
 
-@app.route("/",methods = ["GET","POST"])
-def index():
-    #return "Starting Maching Learning Project"
-    return "CI/CD pipeline has been established"
-
-
-
-if  __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    main()
